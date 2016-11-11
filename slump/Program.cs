d77818b07723f841da60bf89f0596829e0f06220
alfredks <alfredks@WS-VGY-1941>
2016-11-11 14:19:15 +0100

    vggv

--- slump/Program.cs
+++ slump/Program.cs
@@ -10,6 +10,36 @@ namespace slump
     {
         static void Main(string[] args)
         {
+            /*    Random slump = new Random();
+                int summa = 0;
+
+                for (int i = 0; i <= 10 ; i++)
+                {
+                    int tal = slump.Next(20);
+                    summa = summa + tal;
+                    Console.WriteLine(tal);
+
+                }
+                Console.WriteLine("medelvärdet är " + summa/10); */
+
+            Random slump = new Random();
+            
+            int[] nummer = new int[20];
+
+            for (int i = 0; i < 21; i++)
+            {
+                nummer[i] = slump.Next(15, 31);
+            }
+            Array.Sort(nummer);
+            for (int i = 0; i <21; i++)
+            {
+                Console.WriteLine((nummer[i]) + ",");
+            }
+            Console.WriteLine("median är : " + ((nummer[9] + nummer[10]) / 2));
         }
     }
 }
+    
+
+
+
